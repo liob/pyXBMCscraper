7d5bb4ac1bf226dec0167fd226b4ee1950183dc6
liob <winther@students.uni-mainz.de>
2011-08-18 14:52:09 +0200

    initial import support

--- src/XBMCscraper/__init__.py
+++ src/XBMCscraper/__init__.py
@@ -10,6 +10,7 @@ multiple buffers ($$1$$2)
 $INFO[language]
 <url></url> spoof post
 """
+from orca.speech import __resolveACSS
 
 __version__ = '0.1-dev'
 __scraper_framework__ = 1.1
@@ -65,20 +66,49 @@ class Scraper(object):
     required for all scrapers.
     """
     
-    xml = ElementTree()          #: holds the scraper regex xml tree
-    addonxml = ElementTree()     #: xml tree of addon.xml
-    settingsxml= ElementTree()   #: xml tree of resources/settings.xml
-    path = None                  #: filesystem path to scraper
+    xml = ElementTree()          # holds the scraper regex xml tree
+    addonxml = ElementTree()     # xml tree of addon.xml
+    settingsxml= ElementTree()   # xml tree of resources/settings.xml
+    path = None                  # filesystem path to scraper
+    basepath = None              # the path which contains the scraper
+    requires = []                # an array of dicts with the keys: scrpaer, version
+    deps = []                    # contains the dependencies of the scraper as Scraper object
     
     def __init__(self, scraperPath = None):
+        self.requires = []
+        self.deps = []
         if scraperPath:
+            scraperPath = os.path.normpath(scraperPath)
             self.addonxml.parse( os.path.join(scraperPath, "addon.xml") )
             xmlpath = self.addonxml.find("extension").attrib["library"]
             self.xml.parse( os.path.join(scraperPath, xmlpath) )
             self.settingsxml.parse( os.path.join(scraperPath, "resources/settings.xml") )
+            requires = self.addonxml.find("requires")
+            if requires:
+                for require in requires.findall("import"):
+                    self.requires.append({})
+                    self.requires[-1]["scraper"] = require.attrib["addon"]
+                    self.requires[-1]["version"] = require.attrib["version"]
+            else:
+                logging.warning("could not find <requires> in %s/addon.xml" % scraperPath)
+            self.basepath = os.path.split(scraperPath)[0]
         self.path = scraperPath
+        if hasattr(self, "__stage2__"):
+            self.__stage2__()
+        
+    def __stage2__(self):
+        """ a customizable appendix to __init__ """
+        
+    def __resolve_deps__(self):
+        """ resolve import deps of the scraper """
+        for require in self.requires:
+            logging.info("importing scraper %s as dependency." % require["scraper"])
+            scraper = MovieScraper(os.path.join(self.basepath, require["scraper"]))
 
 class MovieScraper(Scraper):
+    
+    def __stage2__(self):
+        self.__resolve_deps__()
     
     def __CreateSearchUrl__(self, querystring):
         """ generates a url for the given querystring """
@@ -116,7 +146,7 @@ class MovieScraper(Scraper):
         results_xml = ElementTree_fromstring(results)
         rv = []
         for result in results_xml.findall("entity"):
-            rv.append({})
+            rv.append(SearchResult())
             for tag in ["title", "id", "year", "url"]:
                 subelement = result.find(tag)
                 if subelement is not None:
